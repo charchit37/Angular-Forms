762f0add5be4dcedded5742b206c6dc48f60c458
charchit37 <70464202+charchit37@users.noreply.github.com>
2020-09-07 02:02:32 +0530

    Update forms.component.ts

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormBuilder,Validators,FormControl} from '@angular/forms';
-import {customValidationEmail,passwordEven} from '../create-employee';
+import {customValidationEmail,passwordEven} from '../../app/create-employee';
 
 
 @Component({
